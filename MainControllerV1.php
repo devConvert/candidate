941d9c59cd8f7f3cb64dd9bbfd765df3621961c3
devConvert <40272125+devConvert@users.noreply.github.com>
2019-04-04 16:56:51 +0300

    Create MainControllerV1.php

---
+++ MainControllerV1.php
@@ -0,0 +1,29 @@
+<?php
+
+class MainControllerV1 extends BaseControllerV1{
+	
+    public function test(){
+        die("test");
+    }
+
+    public function test2($p1, $p2){
+        die($p1 . $p2);
+    }
+
+    public function get_users(){
+
+
+        $db = $this->db_connect("candidate");
+
+        $sql = "select * from candidate.users";
+
+        $results = $db->query($sql);
+
+        echo "<pre>";
+        print_r($results);
+        die();
+
+
+	  }
+  
+}
